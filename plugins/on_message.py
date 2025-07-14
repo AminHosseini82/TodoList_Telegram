9e0f788b6a77444f75bbd99dbf7498c411703773
Amin Hosseini <aminhosseini822003@gmail.com>
2025-07-14 20:06:23 +0330

    tesr reply_text

--- plugins/on_message.py
+++ plugins/on_message.py
@@ -7,9 +7,7 @@ def on_message(client: Client, message: Message):
     message.reply_text("this message isn't in app and from on_message app",
                        reply_markup=ReplyKeyboardMarkup
                         ([
-                           ["سلام test 1"],
-                           ["<UNK> test 2"],
-
+                           ["سلام test 1" , "asasasas"],
                        ],
                        resize_keyboard = True,
                        one_time_keyboard = False,))
