53434f4a4e57f40b18993a8274523a9534defe75
Amin Hosseini <aminhosseini822003@gmail.com>
2025-07-14 20:25:12 +0330

    reply_markup=InlineKeyboardMarkup

--- plugins/on_message.py
+++ plugins/on_message.py
@@ -1,18 +1,15 @@
+from tkinter.font import names
+
 from pyrogram import Client, filters
-from pyrogram.types import Message, KeyboardButton, ReplyKeyboardMarkup
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 
 
 @Client.on_message()
 def on_message(client: Client, message: Message):
     message.reply_text("this message isn't in app and from on_message app",
-                       reply_markup=ReplyKeyboardMarkup(
+                       reply_markup=InlineKeyboardMarkup(
                            [
-                               [
-                                   "ammin" , "kskskksks"
-                               ],
-                               ["far"],
-                               [
-                                   "asdasdasd", "asdasdasdasd"
-                               ]
+                               [InlineKeyboardButton(text = "amin" , callback_data = "amin hosseini") , InlineKeyboardButton(text = "da" , callback_data = "ds hosseini")],
+                               [InlineKeyboardButton(text = "fa" , callback_data = "aminss hosseini")],
                            ]
                        ))
