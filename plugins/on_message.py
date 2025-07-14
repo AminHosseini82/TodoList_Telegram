f4a3c9aa1cca37d260db8069fe32c2ac841b3ca2
Amin Hosseini <aminhosseini822003@gmail.com>
2025-07-14 20:14:25 +0330

    ReplyKeyboardMarkup

--- plugins/on_message.py
+++ plugins/on_message.py
@@ -5,9 +5,14 @@ from pyrogram.types import Message, KeyboardButton, ReplyKeyboardMarkup
 @Client.on_message()
 def on_message(client: Client, message: Message):
     message.reply_text("this message isn't in app and from on_message app",
-                       reply_markup=ReplyKeyboardMarkup
-                        ([
-                           ["سلام test 1" , "asasasas"],
-                       ],
-                       resize_keyboard = True,
-                       one_time_keyboard = False,))
+                       reply_markup=ReplyKeyboardMarkup(
+                           [
+                               [
+                                   "ammin" , "kskskksks"
+                               ],
+                               ["far"],
+                               [
+                                   "asdasdasd", "asdasdasdasd"
+                               ]
+                           ]
+                       ))
