0696887d7976a10d09d2ef456b8233a080b9fe02
Amin Hosseini <aminhosseini822003@gmail.com>
2025-07-15 21:54:22 +0330

    see client in this file

--- plugins/on_message.py
+++ plugins/on_message.py
@@ -8,10 +8,6 @@ from pyrogram.enums import ChatAction
 @Client.on_message()
 def on_message(client: Client, message: Message):
     message.reply_chat_action(action=ChatAction.TYPING)
-    if message.text == "خلیج عربی":
-        client.send_message(chat_id=7887229801, text="خلیج فارس")
-        message.reply_chat_action
-    else:
-        client.send_message(chat_id=7887229801, text=message.text)
+    client.send_message(chat_id=7887229801, text=message.text)
 
 
