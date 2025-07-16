f8fcd71b3a8363f3382c37272575883ed9c5c8d7
Amin Hosseini <aminhosseini822003@gmail.com>
2025-07-16 19:31:00 +0330

    client.send_message(chat_id=7887229801, text=client.copy_message())

--- plugins/on_message.py
+++ plugins/on_message.py
@@ -7,6 +7,6 @@ from pyrogram.enums import ChatAction
 @Client.on_message()
 def on_message(client: Client, message: Message):
     message.reply_chat_action(action=ChatAction.TYPING)
-    client.send_message(chat_id=7887229801, text=client.get_chat_history(chat_id=7887229801))
+    client.send_message(chat_id=7887229801, text=client.copy_message())
 
 
