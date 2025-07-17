458585e2325afbe46b0abe813c263a4e25d61534
Amin Hosseini <aminhosseini822003@gmail.com>
2025-07-17 07:15:12 +0330

    async def start_handler(client: Client, message: Message):

--- plugins/on_message.py
+++ plugins/on_message.py
@@ -1,10 +1,9 @@
-from pyrogram import Client
+from pyrogram import Client, filters
 from pyrogram.types import Message
 from pyrogram.enums import ChatAction
-import pyromod.listen
 
 
-@Client.on_message()
+@Client.on_message(~filters.command("start") & filters.text)
 async def on_message(client: Client, message: Message):
     await message.reply_chat_action(action=ChatAction.TYPING)
     await message.reply_text(f"your message: {message.text}")
--- plugins/start.py
+++ plugins/start.py
@@ -2,18 +2,11 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 import pyromod.listen
 
-from __main__ import app
 
-
-@app.on_message(filters.command("start"))
+@Client.on_message(filters.command("start"))
 async def start_handler(client: Client, message: Message):
-    answer = await client.ask(message.from_user.id, "Send me your name:")
-    await client.send_message(message.from_user.id, f"Your name: {answer.text}")
-
-# @Client.on_message(filters.command("start"))
-# async def start_handler(client: Client, message: Message):
-#     answer = await client.ask(message.from_user.id, "send my your name:")
-#     await client.send_message(message.from_user.id, f"your name: {answer.text}")
+    answer = await client.ask(message.from_user.id, "send my your name:")
+    await client.send_message(message.from_user.id, f"your name: {answer}")
 
 
 # this is test on message
