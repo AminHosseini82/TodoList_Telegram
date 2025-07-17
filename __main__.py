91bacf31f7a256adeff1f089901b7b28b2abcca3
Amin Hosseini <aminhosseini822003@gmail.com>
2025-07-17 09:27:44 +0330

    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7711350201:AAFxdi3bpLtaCteWFb0KtD93BcFLIXEkXb8")

--- __main__.py
+++ __main__.py
@@ -1,14 +1,19 @@
+import os
 from pyrogram import Client, filters
 from pyrogram.types import Message
 import pyromod.listen
 
 plugins = dict(root="plugins")
 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7711350201:AAFxdi3bpLtaCteWFb0KtD93BcFLIXEkXb8")
+APP_ID = os.environ.get("APP_ID", "21980055")
+API_HASH = os.environ.get("API_HASH", "4ec86b7ffa0214843ef9ba7ad4442ffc")
+
 app = Client(
     name="just-test",
-    api_id="21980055",
-    api_hash="4ec86b7ffa0214843ef9ba7ad4442ffc",
-    bot_token="7711350201:AAFxdi3bpLtaCteWFb0KtD93BcFLIXEkXb8",
+    api_id=APP_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
     plugins=plugins
 )
 
