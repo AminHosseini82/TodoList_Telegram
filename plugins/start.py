6d603f54ece6bb27ac604f6e263014b3bd39ccf2
Amin Hosseini <aminhosseini822003@gmail.com>
2025-07-20 09:52:44 +0330

    just comment some thing that never want

--- plugins/start.py
+++ plugins/start.py
@@ -9,14 +9,14 @@ async def start_handler(client: Client, message: Message):
     await client.send_message(message.from_user.id, f"your name: {answer}")
 
 
-# this is test on message
-@Client.on_message(filters.regex("سلام"))
-async def amin_message(cilent, message: Message):
-    await message.reply_text(f"this is the message.id: {message.id}")
-    await message.reply_text(f"information about User: {message}")
+# # this is test on message
+# @Client.on_message(filters.regex("سلام"))
+# async def amin_message(cilent, message: Message):
+#     await message.reply_text(f"this is the message.id: {message.id}")
+#     await message.reply_text(f"information about User: {message}")
 
 
-@Client.on_message(filters.command('form'))
-async def on_form(client: Client, message: Message):
-    response = await client.listen(chat_id=message.from_user.id)
-    await message.reply_text(response.text)
+# @Client.on_message(filters.command('form'))
+# async def on_form(client: Client, message: Message):
+#     response = await client.listen(chat_id=message.from_user.id)
+#     await message.reply_text(response.text)
