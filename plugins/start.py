bd602ec098f14c542fbe171c3d8db824d01b85a3
Amin Hosseini <aminhosseini822003@gmail.com>
2025-07-21 09:18:49 +0330

    donsnot work

--- plugins/start.py
+++ plugins/start.py
@@ -1,9 +1,27 @@
 from collections import defaultdict
 from pyrogram import Client, filters
-from pyrogram.types import Message
+from pyrogram.enums import ChatMemberStatus
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 import json
 import pyromod.listen
 
+async def join_channels   (client: Client, message: Message):
+    try:
+        member = await client.get_chat_member("amin_test_1", "me")
+
+        if member.status not in [ChatMemberStatus.MEMBER, ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+            raise Exception("Not a member")
+    except:
+        keyboard = InlineKeyboardMarkup(
+            [[InlineKeyboardButton("Join in Channel📢", url="amin_test_1")]]
+        )
+        await message.reply_text(
+            "You must join in channel to use the bot",
+            reply_markup=keyboard
+        )
+        return
+
+join_filter = filters.create(join_channels)
 
 def Tree():
     return defaultdict(Tree)
@@ -25,7 +43,7 @@ async def message_handler(client: Client, message: Message):
         await message.reply_text("your info save in database")
 
 
-@Client.on_message(filters.command("start"))
+@Client.on_message(filters.command("start"),filters.command("join_filter"))
 async def start_handler(client: Client, message: Message):
     await message.reply_text("سلام نام خودت را وارد کنید.")
     user_pocket[message.from_user.id]["step"] = 1  # userpocket = { "65165161" : {'step' = 1}}
