cb86962faf3614f38e3447742d2b857cb6c12c7a
Amin Hosseini <aminhosseini822003@gmail.com>
2025-07-21 09:37:40 +0330

    this code will storm the chanel join in telegram

--- plugins/start.py
+++ plugins/start.py
@@ -5,23 +5,34 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 import json
 import pyromod.listen
 
-async def join_channels   (client: Client, message: Message):
-    try:
-        member = await client.get_chat_member("amin_test_1", "me")
 
-        if member.status not in [ChatMemberStatus.MEMBER, ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
-            raise Exception("Not a member")
-    except:
+async def join_channels(client: Client, message: Message):
+    channel_username = "amin_test_1"  # Better: Store in config
+    try:
+        member = await client.get_chat_member(channel_username, message.from_user.id)
+
+        if member.status not in [
+            ChatMemberStatus.MEMBER,
+            ChatMemberStatus.OWNER,
+            ChatMemberStatus.ADMINISTRATOR
+        ]:
+            raise ValueError("Not a member")
+    except Exception as e:
         keyboard = InlineKeyboardMarkup(
-            [[InlineKeyboardButton("Join in Channel📢", url="amin_test_1")]]
+            [[InlineKeyboardButton("Join Channel 📢", url=f"https://t.me/{channel_username}")]]
         )
         await message.reply_text(
-            "You must join in channel to use the bot",
+            "You must join the channel to use this bot.",
             reply_markup=keyboard
         )
-        return
+        return False  # Explicitly return False to block the command
+    return True  # Allow the command if the user is a member
+
 
 join_filter = filters.create(join_channels)
+
+
+# -------------------------------------------------------------------------------------------------------
 
 def Tree():
     return defaultdict(Tree)
@@ -43,7 +54,13 @@ async def message_handler(client: Client, message: Message):
         await message.reply_text("your info save in database")
 
 
-@Client.on_message(filters.command("start"),filters.command("join_filter"))
+@Client.on_message(filters.command("start"))
 async def start_handler(client: Client, message: Message):
+    # Check if the user has joined the channel
+    is_member = await join_channels(client, message)
+
+    if not is_member:
+        return  # Stop execution if the user hasn't joined
+
     await message.reply_text("سلام نام خودت را وارد کنید.")
-    user_pocket[message.from_user.id]["step"] = 1  # userpocket = { "65165161" : {'step' = 1}}
+    user_pocket[message.from_user.id]["step"] = 1
