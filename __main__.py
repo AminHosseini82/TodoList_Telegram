8602cb3347cc7a033b3fccf00465f2a7af387593
Amin Hosseini <aminhosseini822003@gmail.com>
2025-07-22 13:29:45 +0330

    Just a hi world

--- __main__.py
+++ __main__.py
@@ -7,7 +7,7 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 plugins = dict(root="plugins")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7711350201:AAFxdi3bpLtaCteWFb0KtD93BcFLIXEkXb8")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7200426226:AAHCAjJfeldc3AnnhFqN1mIFIiyFFuDunVQ")
 APP_ID = os.environ.get("APP_ID", "21980055")
 API_HASH = os.environ.get("API_HASH", "4ec86b7ffa0214843ef9ba7ad4442ffc")
 
--- plugins/start.py
+++ plugins/start.py
@@ -1,7 +1,7 @@
-# from collections import defaultdict
-# from pyrogram import Client, filters
-# from pyrogram.enums import ChatMemberStatus
-# from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-# import json
-# import pyromod.listen
-# from pyrogram.types.bots_and_keyboards import callback_query
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram import Client, filters, enums
+
+@Client.on_message(filters.command(["start"]))
+async def start_handler(client: Client, message: Message):
+    await message.reply_chat_action(enums.ChatAction.TYPING)
+    await message.reply_text("Hi world")
