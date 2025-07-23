fb19622c3b425fb6c400ccbd56690b3879acf0e0
Amin Hosseini <aminhosseini822003@gmail.com>
2025-07-23 11:28:08 +0330

    testing InlinekeyboardMarkup

--- plugins/start.py
+++ plugins/start.py
@@ -1,4 +1,4 @@
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, ReplyKeyboardMarkup
 from pyrogram import Client, filters, enums
 from database.models import User, session
 
@@ -12,11 +12,11 @@ async def start_handler(client: Client, message: Message):
     user_last_name = message.from_user.last_name
 
     # exsisting_user = session.query(User).filter(User.id == user_id).first()
-    existing_user = session.query(User).filter_by(user_id = user_id).first()
+    existing_user = session.query(User).filter_by(user_id=user_id).first()
 
     # checking user is new or old one.
     if not existing_user:  # user is new
-        new_user = User(user_id = user_id, firstname=user_first_name, lastname=user_last_name)
+        new_user = User(user_id=user_id, firstname=user_first_name, lastname=user_last_name)
         new_user.set_password("123456")
         session.add(new_user)
         session.commit()
@@ -27,8 +27,30 @@ async def start_handler(client: Client, message: Message):
         await message.reply_chat_action(enums.ChatAction.TYPING)
         await message.reply_text("خوش اومدی!")
 
+    # Show a button
+    await message.reply_chat_action(enums.ChatAction.TYPING)
+    await message.reply_text("میخوای چی کار بکنی؟", reply_markup=InlineKeyboardMarkup(
+        [
+            [  # First row
+                InlineKeyboardButton(  # Generates a callback query when pressed
+                    "Button",
+                    callback_data="data"
+                ),
+                InlineKeyboardButton(  # Opens a web URL
+                    "URL",
+                    url="https://docs.pyrogram.org"
+                ),
+            ],
+            [  # Second row
+                InlineKeyboardButton(  # Opens the inline interface
+                    "Choose chat",
+                    switch_inline_query="pyrogram"
+                ),
+                InlineKeyboardButton(  # Opens the inline interface in the current chat
+                    "Inline here",
+                    switch_inline_query_current_chat="pyrogram"
+                )
+            ]
+        ]
 
-
-
-
-
+    ))
