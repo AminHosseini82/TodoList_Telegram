0ac277d0494977c030d5fe63851dc71ad4339659
Amin Hosseini <aminhosseini822003@gmail.com>
2025-07-23 10:01:15 +0330

    Get user informations and write it in database

---
+++ database/models.py
@@ -0,0 +1,55 @@
+import bcrypt
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///database.db', echo=True)
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(String)  # user_id will unique=True in Telegram
+    firstname = Column(String(100))
+    lastname = Column(String(100))
+    password = Column(String(100))
+    # relationship with Todo_class
+    todos = relationship("Todo", back_populates="users")
+
+    def __repr__(self):
+        return f"<User {self.user_id}>"
+
+    def set_password(self, password):
+        # Generate a salt (random string)
+        salt = bcrypt.gensalt()
+        # Encode the password string to bytes (UTF-8 is common)
+        password_bytes = password.encode('utf-8')
+        # Hash the password with the salt
+        self.password = bcrypt.hashpw(password_bytes, salt)
+
+    def check_password(self, password):
+        return bcrypt.checkpw(password, self.password)
+
+
+class Todo(Base):
+    __tablename__ = "todos"
+    id = Column(Integer, primary_key=True)
+    title = Column(String(300))
+    description = Column(String)
+    # the ForeignKey to user
+    user_id = Column(Integer, ForeignKey('users.id'))  # کلید خارجی به جدول users
+    # relationship with users
+    users = relationship("User", back_populates="todos")
+
+    def __repr__(self):
+        return f"title: {self.title}"
+
+
+# create database and tables
+Base.metadata.create_all(engine)
+
+# create session for work with database.
+Session = sessionmaker(bind=engine)
+session = Session()
--- plugins/start.py
+++ plugins/start.py
@@ -1,7 +1,34 @@
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram import Client, filters, enums
+from database.models import User, session
+
 
 @Client.on_message(filters.command(["start"]))
 async def start_handler(client: Client, message: Message):
     await message.reply_chat_action(enums.ChatAction.TYPING)
-    await message.reply_text("Hi world")
+    # await message.reply_text("Hi world")
+    user_id = message.from_user.id
+    user_first_name = message.from_user.first_name
+    user_last_name = message.from_user.last_name
+
+    # exsisting_user = session.query(User).filter(User.id == user_id).first()
+    existing_user = session.query(User).filter_by(user_id = user_id).first()
+
+    # checking user is new or old one.
+    if not existing_user:  # user is new
+        new_user = User(user_id = user_id, firstname=user_first_name, lastname=user_last_name)
+        new_user.set_password("123456")
+        session.add(new_user)
+        session.commit()
+        await message.reply_chat_action(enums.ChatAction.TYPING)
+        await message.reply_text("کاربر جدید ثبت شد!")
+
+    else:
+        await message.reply_chat_action(enums.ChatAction.TYPING)
+        await message.reply_text("خوش اومدی!")
+
+
+
+
+
+
