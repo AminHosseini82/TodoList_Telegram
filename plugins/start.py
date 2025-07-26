05a61dccb008fb048ce1100bce50999c5df4918f
Amin Hosseini <aminhosseini822003@gmail.com>
2025-07-26 12:46:50 +0330

    feat(Text landler): Add a function to landle the text

--- plugins/start.py
+++ plugins/start.py
@@ -18,3 +18,19 @@ async def start_handler(client: Client, message: Message):
             ["اضافه کردن کار جدید"],
         ]
     ))
+
+
+
+@Client.on_message(filters.text)
+async def text_handler(client: Client, message: Message):
+    # if he didn't start the bot, bot shows login successful message.
+    await login(client, message)
+
+    # Show a button to see all work
+    await message.reply_chat_action(enums.ChatAction.TYPING)
+    await message.reply_text("میخوای چی کار بکنی؟", reply_markup=ReplyKeyboardMarkup(
+        [
+            ["نمایش لیست کار ها"],
+            ["اضافه کردن کار جدید"],
+        ]
+    ))
