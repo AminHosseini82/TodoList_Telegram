2ac6e3fd0b932b6b7c9291ed7a0e908295ab2dfb
Amin Hosseini <aminhosseini822003@gmail.com>
2025-07-26 10:45:28 +0330

    feat(Edit Todos): It can Edit Todos now

--- plugins/Todo/todo_list_view.py
+++ plugins/Todo/todo_list_view.py
@@ -1,7 +1,9 @@
 from pyrogram import filters, client, Client, enums
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from database.models import User, session
+from database.models import User, session, Todo
 from plugins.login_check import login
+from pyromod import listen
+
 
 @Client.on_message(filters.regex("نمایش لیست کار ها"))
 async def todo_list(client: Client, message: Message):  # Show all user todolist
@@ -40,3 +42,32 @@ async def todo_list(client: Client, message: Message):  # Show all user todolist
                 ]
             ))
 
+
+@Client.on_callback_query()
+async def edit_todo(client: Client, callback_query):
+    # When someone press Edit_todo
+    if callback_query.data == "Edit_todo":
+        # Get new data
+        user_id = callback_query.from_user.id
+
+        user_object = session.query(User).filter_by(user_id=user_id).first()
+        old_todo = session.query(Todo).filter_by(users=user_object).first()
+
+        if old_todo:
+            # Ask for new title and new description
+            new_title = await client.ask(user_id, "لطفا عنوان جدید خود را وارد بکنید.")
+            new_description = await client.ask(user_id, "حالا لطفا توضیحات جدید خودتون رو وارد بکنید.")
+
+            # Add old title and description to new one
+            old_todo.title = new_title.text
+            old_todo.description = new_description.text
+
+            # Add to database
+            session.add(old_todo)
+            session.commit()
+
+            # show successful message
+            await callback_query.message.reply_text("تغییرات جدید title و description جدید شما ذخیره شد.")
+
+        else:
+            await callback_query.message.reply_text("اصلا شما همجین Todo ای ندارین.")
